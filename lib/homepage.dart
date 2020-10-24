fc312f25e179e1e7b8bb9f0843211035e86ea5b2
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-10-24 15:20:51 +0700

    tambah display name, pp, text welcome and lets learn about UI Design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,21 +3,56 @@ import 'package:flutter/material.dart';
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Design UI Course'),
-        backgroundColor: Colors.orange,
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Kelompok A3M'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.orange,
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
-    );
+    return Container(
+        color: Colors.white,
+        child: Scaffold(
+          body: Container(
+            padding: EdgeInsets.only(top: 40, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Arif Budiman Arrosyid",
+                          style: TextStyle(
+                            //backgroundColor: Colors.blue,
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Text(
+                          "Welcome to Adobe XD Course",
+                          style: TextStyle(fontSize: 12, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage('assets/icon/pp.png'))),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Lets learn about UI Design',
+                  style: TextStyle(
+                      fontSize: 15, height: 1.3, fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
