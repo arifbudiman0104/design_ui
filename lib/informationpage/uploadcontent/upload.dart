b39d325eec1d6524fa4081ebf40084c9fc6c1f58
Musafak <musafak.ft18@mail.umy.ac.id>
2020-10-26 20:38:51 +0700

    Add Uplode Follow the step carefully

--- lib/informationpage/uploadcontent/upload.dart
+++ lib/informationpage/uploadcontent/upload.dart
@@ -1,17 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class Upload extends StatelessWidget {
+class Upload extends StatefulWidget {
+  @override
+  _UploadState createState() => _UploadState();
+}
+
+class _UploadState extends State<Upload> {
+  String videoURL =
+      "https://www.youtube.com/watch?v=PSoPr9ccogI&t=1302s&ab_channel=ArifBudiman";
+  YoutubePlayerController _controller;
+  @override
+  void initState() {
+    _controller = YoutubePlayerController(
+        initialVideoId: YoutubePlayer.convertUrlToId(videoURL));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("What is UI?"),
-      ),
       body: Container(
-        child: Container(
-          child: Text("ini isi whats is UI page"),
+        padding: EdgeInsets.only(top: 40, left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Text(
+                          "Follow the step carefully",
+                          style: TextStyle(
+                            //backgroundColor: Colors.blue,
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    YoutubePlayerBuilder(
+                      player: YoutubePlayer(
+                        controller: _controller,
+                      ),
+                      builder: (context, player) {
+                        return Column(
+                          children: [player],
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("Follow the step carefully"),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
