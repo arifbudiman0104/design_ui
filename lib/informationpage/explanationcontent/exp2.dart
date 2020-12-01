f32045022edfab98c30d07cc58d4b7704f0313d8
Abdul eriawan nahar <abdul.eriawan.ft18@mail.umy.ac.id>
2020-12-01 15:12:21 +0700

    add explanation 2

--- lib/informationpage/explanationcontent/exp2.dart
+++ lib/informationpage/explanationcontent/exp2.dart
@@ -7,8 +7,7 @@ class Exp2 extends StatefulWidget {
 }
 
 class _Exp2State extends State<Exp2> {
-  String videoURL =
-      "https://www.youtube.com/watch?v=PSoPr9ccogI&t=1302s&ab_channel=ArifBudiman";
+  String videoURL = "https://youtu.be/GzkpQL8mYQY";
   YoutubePlayerController _controller;
   @override
   void initState() {
